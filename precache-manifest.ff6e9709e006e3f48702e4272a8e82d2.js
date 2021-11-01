9e3b43188c7c3bb8be9daf4809bec303cdcbcf62
Harshsinghs1058 <harshsinghs1058@gmail.com>
2021-11-01 13:54:12 +0530

    Updates

---
+++ precache-manifest.ff6e9709e006e3f48702e4272a8e82d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e36bdc4d29d361304dc033f5e8d901",
+    "url": "/todo_web_mern_frontend/index.html"
+  },
+  {
+    "revision": "4b109d3fddbbaf4eb8a7",
+    "url": "/todo_web_mern_frontend/static/css/main.da8c2a34.chunk.css"
+  },
+  {
+    "revision": "7e16a2e567b17c8651b1",
+    "url": "/todo_web_mern_frontend/static/js/2.952b8562.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/todo_web_mern_frontend/static/js/2.952b8562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b109d3fddbbaf4eb8a7",
+    "url": "/todo_web_mern_frontend/static/js/main.6423ffd0.chunk.js"
+  },
+  {
+    "revision": "82af10c76c7dcf21a6da",
+    "url": "/todo_web_mern_frontend/static/js/runtime-main.2287756b.js"
+  },
+  {
+    "revision": "0266b5a38f52b0c7f8f8fd9186624cbe",
+    "url": "/todo_web_mern_frontend/static/media/study.0266b5a3.png"
+  },
+  {
+    "revision": "dec782572737d9127958295bf75c2502",
+    "url": "/todo_web_mern_frontend/static/media/study.dec78257.gif"
+  }
+]);
